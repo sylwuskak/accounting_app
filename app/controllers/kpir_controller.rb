2ebb0cdf84c8d28d3a5eab83f9658eec3ed62338
sylwuskak <sylwuskak@gmail.com>
2020-01-19 14:59:12 +0100

    kpir fixes

--- app/controllers/kpir_controller.rb
+++ app/controllers/kpir_controller.rb
@@ -2,7 +2,7 @@ class KpirController < ApplicationController
 
   def get_kpir
     if params[:date]
-      @operations = current_user.operations.select{|o| o.date.year == params[:date].to_i}.sort_by{|o| o.date}
+      @operations = current_user.operations.select{|o| o.date.year == params[:date].to_i}.group_by{|o| [o.date, o.invoice_no]}.sort_by{|k, v| k[0]}
     end
   end
 
