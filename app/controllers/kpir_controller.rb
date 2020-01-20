2da4ab3c7bc4fc9d2001208f52f2753f62d7d096
sylwuskak <sylwuskak@gmail.com>
2020-01-20 09:04:53 +0100

    kpir order fix

--- app/controllers/kpir_controller.rb
+++ app/controllers/kpir_controller.rb
@@ -4,7 +4,7 @@ class KpirController < ApplicationController
     if params[:date]
       # require 'pry'
       # binding.pry
-      @operations = current_user.operations.select{|o| o.date.year == params[:date].to_i}.group_by{|o| [o.date, o.invoice_no]}.sort_by{|k, v| [k[0], -v.first.contractor_name] }
+      @operations = current_user.operations.select{|o| o.date.year == params[:date].to_i}.group_by{|o| [o.date, o.invoice_no]}.sort_by{|k, v| [k[0], -v.first.amount] }
     end
   end
 
